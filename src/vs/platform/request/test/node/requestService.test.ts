331db2651cf531104adb03f042923ec579f28c4c
Christof Marti <chrmarti@microsoft.com>
2024-11-26 09:27:35 +0100

    Test kerberos glue

---
+++ src/vs/platform/request/test/node/requestService.test.ts
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import assert from 'assert';
+import { ensureNoDisposablesAreLeakedInTestSuite } from '../../../../base/test/common/utils.js';
+import { NullLogService } from '../../../log/common/log.js';
+import { lookupKerberosAuthorization } from '../../node/requestService.js';
+
+
+suite('request service', () => {
+	const store = ensureNoDisposablesAreLeakedInTestSuite();
+
+	test('Kerberos lookup', async () => {
+		try {
+			const logService = store.add(new NullLogService());
+			const response = await lookupKerberosAuthorization('http://localhost:9999', undefined, logService, 'requestService.test.ts');
+			assert.ok(response);
+		} catch (err) {
+			assert.ok(
+				err?.message?.includes('No authority could be contacted for authentication')
+				|| err?.message?.includes('No Kerberos credentials available')
+				|| err?.message?.includes('No credentials are available in the security package')
+				|| err?.message?.includes('no credential for')
+				, `Unexpected error: ${err}`);
+		}
+	});
+});
